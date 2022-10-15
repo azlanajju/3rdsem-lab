3bc5d3c284c838ebbb74d62d244836bfdbbc5fb5
muhammedazlan11@gmail.com <muhammedazlan11@gmail.com>
2022-10-15 10:57:21 +0530

    c

--- main.js
+++ main.js
@@ -3,32 +3,7 @@
         name: 'WELCOME',
         message: '           -by azlan',
     });  */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
-function onn() {
-  document.getElementById("overlay").style.display = "block";
-}
-
-function offf() {
-  document.getElementById("overlay").style.display = "none";
-}
 
 function myFunction8() {
   
