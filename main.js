f77a4602b916df38d76af26d3733c51c896a8aed
Azlan <98301786+azlanajju@users.noreply.github.com>
2023-03-15 14:12:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -48,3 +48,53 @@ function myFunction4() {
    copyText.select(); 
    navigator.clipboard.writeText(copyText.value);
  }
+
+ function myFunction6() {
+  document.getElementById("copied5").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput5");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction7() {
+  document.getElementById("copied6").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput6");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction8() {
+  document.getElementById("copied7").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput7");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction9() {
+  document.getElementById("copied8").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput8");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction10() {
+  document.getElementById("copied9").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput9");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction11() {
+  document.getElementById("copied10").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput10");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
+ function myFunction12() {
+  document.getElementById("copied11").innerHTML = "Copied!";
+ 
+   var copyText = document.getElementById("myInput11");
+   copyText.select(); 
+   navigator.clipboard.writeText(copyText.value);
+ }
