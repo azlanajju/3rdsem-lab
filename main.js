be1c2704b4272af4995ef3270fe0bedef63d7e12
Azlan <98301786+azlanajju@users.noreply.github.com>
2024-03-22 10:55:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+window.location.href="https://examrestrict.netlify.app/";
+
+
 function upload(){
   document.getElementById('confirm').style.display="block";
 }
@@ -108,4 +111,3 @@ function myFunction4() {
 
 
 
-window.location.href="https://examrestrict.netlify.app/";
